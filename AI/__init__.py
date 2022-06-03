2849d3d394716317b1d0f150360cde70b41a0b30
davidegiarolo <davide.giarolo@vtecrm.com>
2022-06-03 17:41:20 +0200

    .

--- AI/__init__.py
+++
@@ -1,6 +0,0 @@
-#***********************************
-# SPDX-FileCopyrightText: 2009-2020 Vtenext S.r.l. <info@vtenext.com> and KLONDIKE S.r.l. <info@klondike.ai>
-# SPDX-License-Identifier: AGPL-3.0-only
-#***********************************
-
-all = ["ml_methods", "word_embedding"]
