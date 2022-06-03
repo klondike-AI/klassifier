c3376aa4bd1d3d167301d8a095c4805df3587e37
davidegiarolo <davide.giarolo@vtecrm.com>
2022-06-03 17:36:39 +0200

    klassifier first commit

---
+++ AI/__init__.py
@@ -0,0 +1,6 @@
+#***********************************
+# SPDX-FileCopyrightText: 2009-2020 Vtenext S.r.l. <info@vtenext.com> and KLONDIKE S.r.l. <info@klondike.ai>
+# SPDX-License-Identifier: AGPL-3.0-only
+#***********************************
+
+all = ["ml_methods", "word_embedding"]
